5fa7c4db003c4d288243a77c3a3b5cb9c52fc018
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-09-29 21:05:00 -0400

    Update form_0.py
    ideation

--- modules/form_0.py
+++ modules/form_0.py
@@ -1,8 +1,9 @@
+import streamlit as st
+
 def render():
- import streamlit as st
- st.markdown(
-            """
-            <p style='font-size: 20px; margin-bottom: 0;'>Do you want to take the survey? If you want to take please proceed.</p>
-            """,
-            unsafe_allow_html=True
-        )
+    st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>Do you want to take the survey? If you want to take it, please proceed.</p>
+        """,
+        unsafe_allow_html=True
+    )

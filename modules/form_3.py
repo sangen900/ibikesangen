289ed51c589d4dbd10e3cd46e69c84c05798acc4
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-09-29 23:24:09 -0400

    Update form_3.py
    d

--- modules/form_3.py
+++ modules/form_3.py
@@ -1,3 +1,4 @@
 import streamlit as st
 def render():
  st.markdown("<p style='font-size: 20px;'>Pretest: What do you already know about manufacturing?Please do your best to attempt these pretest questions. Do not ask others or look up answers, we simply want to measure your knowledge before the curriculum.  What do you already know about manufacturing?</p>", unsafe_allow_html=True)
+render()

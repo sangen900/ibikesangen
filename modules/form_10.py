6973577e7717b404d4e51f35edf87fe47e09cff4
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-02 15:40:03 -0400

    Update form_10.py

--- modules/form_10.py
+++ modules/form_10.py
@@ -1,6 +1,6 @@
+import streamlit as st
 def render():
- import streamlit as st
     # YouTube video URL
- youtube_url = "https://youtu.be/gbNZ9pVB6-0"
- st.video(youtube_url)
-    render()
+    youtube_url = "https://youtu.be/gbNZ9pVB6-0"
+    st.video(youtube_url)
+    

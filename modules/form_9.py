c9337f629dea27250af4a9cd4602030f75f53b17
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-08 14:46:35 -0400

    Update form_9.py
    updated code

--- modules/form_9.py
+++ modules/form_9.py
@@ -1,69 +1,85 @@
 import streamlit as st
-def render():
-    # Define a CSS style to set the font size to 20 pixels and reduce the margin
-    css_style = f"font-size: 20px; margin: 0;"
-
-    # Define the radio button style to inherit font size
-    radio_style = ".st-radio .stRadio label { font-size: inherit; }"
-
-    # Main Streamlit app
-    st.markdown(
-        """
-        <p style='font-size: 24px; margin-bottom: 0;'><b>Check-in: iBike Lesson Part 2</b></p>
-        """,
-        unsafe_allow_html=True
-    )
-    st.markdown("---")
-    # Define statement sets
-    statement_set_1 = [
-        ("The material used to make a product is limited by (select all that apply):", None),
-        ("Its availability", "availability"),
-        ("Its transportability", "transportability"),
-        ("Its affordability", "affordability"),
-        ("Its processibility", "processibility"),
-        ("Its processibility to meet specification", "processibility_specification")
-    ]
-
-    statement_set_2 = [
-        ("Why do natural materials need to be processed? Select all that apply.", None),
-        ("So that material's shape can change as needed by the product", "shape_change"),
-        ("So that the material is more sanitary", "material_sanitary"),
-        ("So that the material's properties can function the way desired by the product", "material_properties"),
-        ("So that varied materials can be used and assembled into one product", "varied_materials_assembly")
-    ]
-
-    statement_set_3 = [
-        ("What should be considered in selecting the method to process materials? Select all that apply.", None),
-        ("The specifications of the product design", "product_design_specifications"),
-        ("Whether the method has the capacity to process materials demanded by the market", "method_capacity"),
-        ("Whether the method produces too much material waste", "material_waste"),
-        ("Whether the processing method requires more investment in tools, space, etc.", "investment_required")
-    ]
-
-    # Loop through statement sets
-    for index, statement_set in enumerate([statement_set_1, statement_set_2, statement_set_3]):
-        if index > 0:
-            st.markdown("---")  # Add a horizontal rule between statement sets
-
-        if statement_set[0][0]:  # Check if a title is defined
-            st.markdown(
-                f"""
-                <p style='{css_style}'>{statement_set[0][0]}</p>
-                """,
-                unsafe_allow_html=True
-            )
-
-        # Loop through individual statements in the set
-        for statement, key in statement_set[1:]:
-            if statement:  # Check if a statement is defined
-                # Display the statement with custom style
-                st.markdown(f'<p style="{css_style}">{statement}:</p>', unsafe_allow_html=True)
-
-                # Apply the CSS style for radio buttons
-                with st.markdown(f'<style>{radio_style}</style>'):
-                    selected_option = st.radio(
-                        "",
-                        ["Applies", "Does NOT apply"],
-                        key=key,
-                        help=""
-                    )
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+def form_10():
+    with st.form("form10"):
+        st.markdown(
+                    """
+                    <p style='font-size: 24px; margin-bottom: 0;'><b>Check-in: iBike Lesson Part 2</b></p>
+                    """,
+                    unsafe_allow_html=True
+                )
+        st.markdown("---")
+        st.markdown(
+                    """
+                    <p style='font-size: 20px; margin-bottom: 1;'>The material used to make a product is limited by (select all that apply):</p>
+                    """,
+                    unsafe_allow_html=True
+                )
+        text_statements1010 = [
+                            "Its availability",
+                            "Its transportability",
+                            "Its affordability",
+                            "Its processibility",
+                            "Its processibility to meet specification."
+                        ]
+        #styling text statements
+        css_style = f"font-size: 20px;"
+        selected_options1010 = []
+        for statement in text_statements1010:
+            st.markdown(f'<div style="{css_style}">{statement}</div>', unsafe_allow_html=True)
+            selected_option1010 = st.radio("", ["Applies", "Does NOT Apply"], index=0, key=statement)
+            selected_options1010.append(selected_option1010) 
+        st.markdown("---")
+        st.markdown(
+                    """
+                    <p style='font-size: 20px; margin-bottom:1;'>Why do natural materials need to be processed? Select all that apply.</p>
+                    """,
+                    unsafe_allow_html=True
+                )
+                    
+        text_statements1011 = [
+                "So that material's shape can change as needed by the product",
+                "So that the material is more sanitary",
+                "So that the material's properties can function the way desired by the product",
+                "So that varied materials can be used and assembled into one product"
+            ]
+        #styling text statements
+        css_style = f"font-size: 20px;"
+        selected_options1011 = []
+        for statement in text_statements1011:
+            st.markdown(f'<div style="{css_style}">{statement}</div>', unsafe_allow_html=True)
+            selected_option1011 = st.radio("", ["Applies", "Does NOT Apply"], index=0, key=statement)
+            selected_options1011.append(selected_option1011)
+        st.markdown("---")
+        st.markdown(
+                    """
+                    <p style='font-size: 20px; margin-bottom: 1;'>What should be considered in selecting the method to process materials? Select all that apply.</p>
+                    """,
+                    unsafe_allow_html=True
+                )
+        text_statements1012 = [
+                "The specifications of the product design",
+                "Whether the method has the capacity to process materials demanded by the market",
+                "Whether the method produces too much material waste",
+                "Whether the processing method requires more investment in tools, space, etc."
+            ]
+        #styling text statements
+        css_style = f"font-size: 20px;"
+        selected_options1012 = []
+        for statement in text_statements1012:
+            st.markdown(f'<div style="{css_style}">{statement}</div>', unsafe_allow_html=True)
+            selected_option1012 = st.radio("", ["Applies", "Does NOT Apply"], index=0, key=statement)
+            selected_options1012.append(selected_option1012) 
+        submit_button10 = st.form_submit_button("Submit Form")
+        if submit_button10:
+            scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+            credentials = ServiceAccountCredentials.from_json_keyfile_name("my-credentials.json", scope)
+            gc = gspread.authorize(credentials)
+            spreadsheet_id = "1UiBuyoFudQnvgzgIxlh__6ktEBKK7zJvqoWYwz_2WuE" 
+            sheet = gc.open_by_key(spreadsheet_id)
+            worksheet_title = "Sheet6"
+            worksheet = sheet.worksheet(worksheet_title)
+            data_to_insert = selected_options1010 + selected_options1011 + selected_options1012
+            worksheet.insert_rows([data_to_insert], len(worksheet.get_all_values()) + 1)
+            st.success("Form 10 has been successfully submitted")

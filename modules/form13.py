9992307066cd1f8cc8da1e558133c8a47d4bca55
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-08 16:56:13 -0400

    Update and rename form_13.py to form13.py

--- modules/form13.py
+++ modules/form13.py
@@ -1,5 +1,5 @@
 import streamlit as st
-def form_14():
+def form_13():
     st.markdown(
             """
             <p style='font-size: 20px; margin-bottom: 0;'>Thank you for participating in survey.</p>

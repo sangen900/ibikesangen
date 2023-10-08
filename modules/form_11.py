69d7cbb69d58388327682d7fab2864847cc0fbb5
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-08 14:49:06 -0400

    Update form_11.py
    updated code

--- modules/form_11.py
+++ modules/form_11.py
@@ -1,147 +1,117 @@
-def render():
-    import streamlit as st
-    # Section 1: Check-in Part 3
-    st.markdown(
-    """
-    <p style='font-size: 24px; margin-bottom: 0;'><b>Check-in: iBike Lesson Part 3</b></p>
-    """,
-    unsafe_allow_html=True
-    )
-    st.markdown("---")
-    # Section 2: Manufacturing Paradigms
-    st.markdown(
-    """
-    <p style='font-size: 20px; margin-bottom: 0;'>Manufacturing paradigms are:</p>
-    """,
-    unsafe_allow_html=True
-    )
-    options_q5 = [
-    "Production Volume", 
-    "Manufacturing equipment", 
-    "Manufacturing technologies", 
-    "Manufacturing models that characterize a specific period", 
-    "Manufacturing methods"
-    ]
+import streamlit as st
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+def form_12():
+    with st.form("form12"):
+        st.markdown(
+        """
+        <p style='font-size: 24px; margin-bottom: 0;'><b>Check-in: iBike Lesson Part 3</b></p>
+        """,
+        unsafe_allow_html=True
+        )
+        st.markdown("---")
+        st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>Manufacturing paradigms are:</p>
+        """,
+        unsafe_allow_html=True
+        )
+        options_q5 = [
+        "Production Volume", 
+        "Manufacturing equipment", 
+        "Manufacturing technologies", 
+        "Manufacturing models that characterize a specific period", 
+        "Manufacturing methods"
+        ]
 
-    selected_option_q5 = st.radio("", options_q5)
+        selected_option_q5 = st.radio("", options_q5)
+        st.markdown("---")
+        st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>The evolution of the manufacturing paradigms is illustrated in the figure below using a volume-variety relationship. Match the letters in the second column to the manufacturing paradigm first column of the table below</p>
+        """,
+        unsafe_allow_html=True
+        )
+        title_manufacturing = ["Lean manufacturing", "Craft production", "Personalized production", "Mass customization", "Mass production"]
+        css_style = f"font-size: 20px;"
+        selected_choices1001 = []
+        for title in title_manufacturing:
+            st.markdown(f'<div style="{css_style}">{title}</div>', unsafe_allow_html=True)
+            selected_choice1001 = st.radio("", ["A", "B/C", "D", "F", "Not Applicable"], index=0, key=title)
+            selected_choices1001.append(selected_choice1001)
+        st.markdown("---")   
+        st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>Which of the following did NOT drive manufacturing technology revolutions? </p>
+        """,
+        unsafe_allow_html=True
+        )
+        options_q1 = ["Steam", "Mechanical assembly", "Electricity", "Internet"]
+        selected_option_q1 = st.radio("", options_q1)
 
-    # Section 3: Evolution of Manufacturing Paradigms
-    st.markdown("---")
+        st.markdown("---")
+        st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>Which of the following supply chain activities may impact a product’s final cost?</p>
+        """,
+        unsafe_allow_html=True
+        )
 
-    st.markdown(
-    """
-    <p style='font-size: 20px; margin-bottom: 0;'>The evolution of the manufacturing paradigms is illustrated in the figure below using a volume-variety relationship. Match the letters in the second column to the manufacturing paradigm first column of the table below</p>
-    """,
-    unsafe_allow_html=True
-    )
-    # Display an image
-    import streamlit as st
-    import os
+        options_q2 = [
+        "Purchasing raw materials from suppliers",
+        "Transporting raw materials to manufacturing plants",
+        "Manufacturing products",
+        "Transporting products to distribution centers",
+        "All of the above"
+        ]
 
-    # Get the user's home directory
-    user_home = os.path.expanduser("~")
+        selected_option_q2 = st.radio("", options_q2)
+        st.markdown("---")
 
-    # Specify the path to the image on your desktop
-    image_filename = "ibike.jpg"
-    desktop_path = os.path.join(user_home, "Desktop", image_filename)
+        st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>Which of the following statements about business processes is true?</p>
+        """,
+        unsafe_allow_html=True
+        )
 
-    # Display the image
-    st.image(desktop_path, use_column_width=True)
+        options_q3 = [
+        "They are executed across multiple functions.",
+        "They are initiated by some type of trigger.",
+        "They involve multiple activities.",
+        "All of the above",
+        "None of the above"
+        ]
 
+        selected_option_q3 = st.radio("", options_q3)
 
-    # Define the list of titles
-    titles = ["Lean manufacturing", "Craft production", "Personalized production", "Mass customization", "Mass production"]
+        st.markdown("---")
+        st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>The tendency of individuals to solely focus on the performance of their own activity with no concern or knowledge of the other tasks associated with proceeding or subsequent activities is called</p>
+        """,
+        unsafe_allow_html=True
+        )
 
-    # Create a dictionary to store selected options for each title
-    selected_options_titles = {}
+        options_q4 = [
+        "The bottleneck effect",
+        "The silo effect",
+        "The blinders effect",
+        "The manufacturing crossover"
+        ]
 
-    # Loop through each title and add radio buttons with unique keys
-    for title in titles:
-    # Display the title
-     st.markdown(
-    f"<p style='font-size: 20px; margin-bottom: 0;'>{title}</p>",
-    unsafe_allow_html=True
-    )
-
-    # Create a unique key for this title
-    key = f"{title}_option"
-
-    # Create a radio button for this title with the options
-    selected_option = st.radio("", ["A", "B/C", "D", "F", "Not Applicable"], key=key)
-
-    # Store the selected option in the dictionary
-    selected_options_titles[title] = selected_option
-
-    # Section 4: Additional Questions
-    st.markdown("---")
-
-    # Question 1
-    st.markdown(
-    """
-    <p style='font-size: 20px; margin-bottom: 0;'>Which of the following did NOT drive manufacturing technology revolutions? </p>
-    """,
-    unsafe_allow_html=True
-    )
-
-    options_q1 = ["Steam", "Mechanical assembly", "Electricity", "Internet"]
-    selected_option_q1 = st.radio("", options_q1)
-
-    st.markdown("---")
-
-    # Question 2
-    st.markdown(
-    """
-    <p style='font-size: 20px; margin-bottom: 0;'>Which of the following supply chain activities may impact a product’s final cost?</p>
-    """,
-    unsafe_allow_html=True
-    )
-
-    options_q2 = [
-    "Purchasing raw materials from suppliers",
-    "Transporting raw materials to manufacturing plants",
-    "Manufacturing products",
-    "Transporting products to distribution centers",
-    "All of the above"
-    ]
-
-    selected_option_q2 = st.radio("", options_q2)
-
-    st.markdown("---")
-
-    # Question 3
-    st.markdown(
-    """
-    <p style='font-size: 20px; margin-bottom: 0;'>Which of the following statements about business processes is true?</p>
-    """,
-    unsafe_allow_html=True
-    )
-
-    options_q3 = [
-    "They are executed across multiple functions.",
-    "They are initiated by some type of trigger.",
-    "They involve multiple activities.",
-    "All of the above",
-    "None of the above"
-    ]
-
-    selected_option_q3 = st.radio("", options_q3)
-
-    st.markdown("---")
-
-    # Question 4
-    st.markdown(
-    """
-    <p style='font-size: 20px; margin-bottom: 0;'>The tendency of individuals to solely focus on the performance of their own activity with no concern or knowledge of the other tasks associated with proceeding or subsequent activities is called</p>
-    """,
-    unsafe_allow_html=True
-    )
-
-    options_q4 = [
-    "The bottleneck effect",
-    "The silo effect",
-    "The blinders effect",
-    "The manufacturing crossover"
-    ]
-
-    selected_option_q4 = st.radio("", options_q4)
-    render()
+        selected_option_q4 = st.radio("", options_q4)
+        
+        submit_button12 = st.form_submit_button("Submit Form")
+        if submit_button12:
+            scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+            credentials = ServiceAccountCredentials.from_json_keyfile_name("my-credentials.json", scope)
+            gc = gspread.authorize(credentials)
+            spreadsheet_id = "1UiBuyoFudQnvgzgIxlh__6ktEBKK7zJvqoWYwz_2WuE" 
+            sheet = gc.open_by_key(spreadsheet_id)
+            worksheet_title = "Sheet7"
+            worksheet = sheet.worksheet(worksheet_title)
+            data_to_insert = [selected_option_q5] + selected_choices1001 + [selected_option_q1, selected_option_q2, selected_option_q3, selected_option_q4]
+            worksheet.insert_rows([data_to_insert], len(worksheet.get_all_values()) + 1)
+            st.success("Form 12 has been successfully submitted")

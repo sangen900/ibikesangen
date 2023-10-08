7eae255c58df81187c2ab90c69d06d3d88baf32c
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-08 18:03:30 -0400

    Update mainform.py

--- modules/mainform.py
+++ modules/mainform.py
@@ -1,18 +1,18 @@
 import streamlit as st
-from form0 import form_0
-from form1 import form_1
-from form2 import form_2
-from form3 import form_3
-from form4 import form_4
-from form5 import form_5
-from form6 import form_6
-from form7 import form_7
-from form8 import form_8
-from form9 import form_9
-from form10 import form_10
-from form11 import form_11
-from form12 import form_12
-from form13 import form_13
+from .form0 import form_0
+from .form1 import form_1
+from .form2 import form_2
+from .form3 import form_3
+from .form4 import form_4
+from .form5 import form_5
+from .form6 import form_6
+from .form7 import form_7
+from .form8 import form_8
+from .form9 import form_9
+from .form10 import form_10
+from .form11 import form_11
+from .form12 import form_12
+from .form13 import form_13
 def main_form():
     page_names1001 = ["form0", "form1", "form2", "form3", "form4", "form5", "form6", "form7", "form8", "form9", "form10", "form11", "form12", "form13"]
     user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", page_names1001)

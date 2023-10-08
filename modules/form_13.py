1da387e2aa07c584d0c19275bd7c9e81e99e3789
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-08 14:52:07 -0400

    Update form_13.py
    update

--- modules/form_13.py
+++ modules/form_13.py
@@ -1,8 +1,9 @@
 import streamlit as st
-def render():
+def form_14():
     st.markdown(
             """
             <p style='font-size: 20px; margin-bottom: 0;'>Thank you for participating in survey.</p>
             """,
             unsafe_allow_html=True
         )
+

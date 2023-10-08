6a2385522588dd9be2dd2d5340582ffdf8d17966
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-08 14:55:48 -0400

    Update mainform.py
    updated

--- modules/mainform.py
+++ modules/mainform.py
@@ -1,61 +1,50 @@
 import streamlit as st
+from form0 import form_1
+from form1 import form_2
+from form2 import form_3
+from form3 import form_4
+from form4 import form_5
+from form5 import form_6
+from form6 import form_7
+from form7 import form_8
+from form8 import form_9
+from form9 import form_10
+from form10 import form_11
+from form11 import form_12
+from form12 import form_13
+from form13 import form_14
 
-# Import your form modules
-from modules import form_0
-from modules import form_1
-from modules import form_2
-from modules import form_3
-from modules import form_4
-from modules import form_5
-from modules import form_6
-from modules import form_7
-from modules import form_8
-from modules import form_9
-from modules import form_10
-from modules import form_11
-from modules import form_12
-from modules import form_13
-
-def render():
-    # Check if "page_index" exists in session state, and initialize it to 0 if not present
-    if "page_index" not in st.session_state:
-        st.session_state.page_index = 0
+def main_form():
+    page_names1001 = ["form0", "form1", "form2", "form3", "form4", "form5", "form6", "form7", "form8", "form9", "form10", "form11", "form12", "form13"]
+    user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", page_names1001)
     
-    # List of form rendering functions
-    pages = [
-        form_0.render,
-        form_1.render,
-        form_2.render,
-        form_3.render,
-        form_4.render,
-        form_5.render,
-        form_6.render,
-        form_7.render,
-        form_8.render,
-        form_9.render,
-        form_10.render,
-        form_11.render,
-        form_12.render,
-        form_13.render,
-    ]
-    
-    # Render the current form based on the page_index
-    pages[st.session_state.page_index]()
-
-    # Create columns for navigation buttons
-    col1, col3 = st.columns([1, 1])
-    
-    # Previous button logic
-    if col1.button("Previous", key="prev_button") and st.session_state.page_index > 0:
-        st.session_state.page_index -= 1
-    
-    # Next button logic
-    if col3.button("Next", key="next_button", disabled=st.session_state.page_index == len(pages) - 1):
-        st.session_state.page_index += 1
-        st.session_state.page_index = max(0, min(st.session_state.page_index, len(pages) - 1))
-
-# # Streamlit app title
-# st.title("Form Navigation")
-
-# # Call the main_form() function when the Streamlit app is run
-# main_form()
+    if user_selected_page == "form1":
+        form_1()
+    elif user_selected_page == "form2":
+        form_2()
+    elif user_selected_page == "form3":
+        form_3()
+    elif user_selected_page == "form4":
+        form_4()
+    elif user_selected_page == "form5":
+        form_5()
+    elif user_selected_page == "form6":
+        form_6()
+    elif user_selected_page == "form7":
+        form_7()
+    elif user_selected_page == "form8":
+        form_8()
+    elif user_selected_page == "form9":
+        form_9()
+    elif user_selected_page == "form10":
+        form_10()
+    elif user_selected_page == "form11":
+        form_11()
+    elif user_selected_page == "form12":
+        form_12()
+    elif user_selected_page == "form13":
+        form_13()
+    elif user_selected_page == "form14":
+        form_14()
+if __name__ == "__main__":
+    main_form()

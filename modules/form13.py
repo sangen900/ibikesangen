4fc50149dfe1c79ebd3329c06c2f2b95a6304ed3
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-13 20:28:33 -0400

    Update form13.py

--- modules/form13.py
+++ modules/form13.py
@@ -1,9 +1,18 @@
 import streamlit as st
+from streamlit import session_state as ss
+
 def form_13():
     st.markdown(
             """
             <p style='font-size: 20px; margin-bottom: 0;'>Thank you for participating in survey.</p>
             """,
-            unsafe_allow_html=True
+            unsafe_allow_html=True    
         )
+    st.button("Finish Survey", on_click=toggle_survey_state)
+        #ss.survey_active = False;
 
+def toggle_survey_state():
+	if 'survey_active' not in ss:
+		ss.survey_active = False;
+
+	ss.survey_active = not(ss.survey_active)

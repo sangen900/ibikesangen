0df608cd82a48cd98f348d57077a0e571353773b
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-19 19:04:35 -0400

    Update mainform.py

--- modules/mainform.py
+++ modules/mainform.py
@@ -16,43 +16,18 @@ from .form11 import form_11
 from .form12 import form_12
 from .form13 import form_13
 
-# Use CSS to style the sidebar layout
-st.write(
-    """
-    <style>
-    .sidebar-content {
-        display: flex;
-        flex-direction: column;
-    }
-    .sidebar-radio-container {
-        display: flex;
-        align-items: center;
-    }
-    .sidebar-radio-label {
-        margin-right: 10px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 def main_form():
     page_names = ["form0", "form1", "form2", "form3", "form4", "form5", "form6", "form7", "form8", "form9", "form10", "form11", "form12", "form13"]
-    form_status = {page: False for page in page_names}
-    
-    user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", page_names)
-    
-    # Check if the selected form is one of the auto-completed forms (form0, form3, form5, form8, form13)
-    auto_completed_forms = ["form0", "form3", "form5", "form8", "form13"]
-    
-    # Display the checkboxes in a more flexible way
-    for page in page_names:
-        form_status[page] = st.sidebar.checkbox(f"{page} Completed", key=page)
 
-    # Check if the selected page is an auto-completed form and automatically check the checkbox
-    if user_selected_page in auto_completed_forms:
-        form_status[user_selected_page] = True
-    
+    user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", page_names)
+
+    # Display the checkboxes and radio buttons together
+    for page in page_names:
+        checkbox_text = f"[ ] {page}"
+        if user_selected_page == page:
+            checkbox_text = f"[x] {page}"
+        st.sidebar.markdown(checkbox_text, unsafe_allow_html=True)
+
     # Display the form corresponding to the selected page
     form_functions = {
         "form0": form_0,
@@ -73,11 +48,6 @@ def main_form():
 
     if user_selected_page in form_functions:
         form_functions[user_selected_page]()
-
-    # Check if the user has submitted the form successfully and update the checkbox
-    if st.button("Submit Form"):
-        if user_selected_page not in auto_completed_forms:
-            form_status[user_selected_page] = True
 
 if __name__ == "__main__":
     main_form()

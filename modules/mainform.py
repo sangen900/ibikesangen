dc566a412bf623760febe7ba01e6ea2f1d4fb241
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-19 18:55:53 -0400

    Update mainform.py

--- modules/mainform.py
+++ modules/mainform.py
@@ -1,30 +1,30 @@
 import streamlit as st
 
-# Import your form functions here without the dot prefix.
-from form0 import form_0
-from form1 import form_1
-from form2 import form_2
-from form3 import form_3
-from form4 import form_4
-from form5 import form_5
-from form6 import form_6
-from form7 import form_7
-from form8 import form_8
-from form9 import form_9
-from form10 import form_10
-from form11 import form_11
-from form12 import form_12
-from form13 import form_13
+# Import your form functions here with the dot prefix.
+from .form0 import form_0
+from .form1 import form_1
+from .form2 import form_2
+from .form3 import form_3
+from .form4 import form_4
+from .form5 import form_5
+from .form6 import form_6
+from .form7 import form_7
+from .form8 import form_8
+from .form9 import form_9
+from .form10 import form_10
+from .form11 import form_11
+from .form12 import form_12
+from .form13 import form_13
 
 def main_form():
     page_names = ["form0", "form1", "form2", "form3", "form4", "form5", "form6", "form7", "form8", "form9", "form10", "form11", "form12", "form13"]
     form_status = {page: False for page in page_names}
     
+    user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", page_names)
+    
     # Create checkboxes in the sidebar for each form
     for page in page_names:
         form_status[page] = st.sidebar.checkbox(f"{page} Completed", key=page)
-    
-    user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", page_names)
     
     # Display the form corresponding to the selected page
     form_functions = {

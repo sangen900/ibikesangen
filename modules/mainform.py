358cc4442b3f5d4a8cf901232a41b2e870d354af
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-19 19:16:34 -0400

    Update mainform.py

--- modules/mainform.py
+++ modules/mainform.py
@@ -1,6 +1,4 @@
 import streamlit as st
-
-# Import your form functions here with the dot prefix.
 from .form0 import form_0
 from .form1 import form_1
 from .form2 import form_2
@@ -16,57 +14,50 @@ from .form11 import form_11
 from .form12 import form_12
 from .form13 import form_13
 
-# Create a dictionary to track form submission status
-form_submission_status = {
-    "form0": False,
-    "form1": False,
-    "form2": False,
-    "form3": False,
-    "form4": False,
-    "form5": False,
-    "form6": False,
-    "form7": False,
-    "form8": False,
-    "form9": False,
-    "form10": False,
-    "form11": False,
-    "form12": False,
-    "form13": False,
-}
-
 def main_form():
-    form_functions = {
-        "form0": form_0,
-        "form1": form_1,
-        "form2": form_2,
-        "form3": form_3,
-        "form4": form_4,
-        "form5": form_5,
-        "form6": form_6,
-        "form7": form_7,
-        "form8": form_8,
-        "form9": form_9,
-        "form10": form_10,
-        "form11": form_11,
-        "form12": form_12,
-        "form13": form_13
-    }
+    page_names = ["form0", "form1", "form2", "form3", "form4", "form5", "form6", "form7", "form8", "form9", "form10", "form11", "form12", "form13"]
     
-    user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", list(form_functions.keys()))
-
-    # Check if the form has been submitted successfully
-    if user_selected_page in form_submission_status and form_submission_status[user_selected_page]:
-        st.sidebar.markdown(f"**{user_selected_page} (Completed)**")
-    else:
-        st.sidebar.radio(user_selected_page, list(form_functions.keys()))
-
-    # Call the selected form function
-    form_functions.get(user_selected_page, lambda: st.write("Select a form from the sidebar."))()
-
-    # Check if the form was submitted and update the status
-    if st.form_submit_button("Submit Form"):
-        if user_selected_page in form_submission_status:
-            form_submission_status[user_selected_page] = True
+    # Create a dictionary to track the checkbox status for each form
+    checkbox_status = {page: False for page in page_names}
+    
+    user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", page_names)
+    
+    # Display the checkboxes and radio buttons in a loop
+    for page in page_names:
+        checkbox_status[page] = st.sidebar.checkbox(f"Completed {page}", key=f"{page}_checkbox")
+    
+    # Check if the user selected a form
+    if user_selected_page:
+        # Display the selected form based on the user's choice
+        st.write(f"Form selected: {user_selected_page}")
+        if user_selected_page == "form0":
+            form_0()
+        elif user_selected_page == "form1":
+            form_1()
+        elif user_selected_page == "form2":
+            form_2()
+        elif user_selected_page == "form3":
+            form_3()
+        elif user_selected_page == "form4":
+            form_4()
+        elif user_selected_page == "form5":
+            form_5()
+        elif user_selected_page == "form6":
+            form_6()
+        elif user_selected_page == "form7":
+            form_7()
+        elif user_selected_page == "form8":
+            form_8()
+        elif user_selected_page == "form9":
+            form_9()
+        elif user_selected_page == "form10":
+            form_10()
+        elif user_selected_page == "form11":
+            form_11()
+        elif user_selected_page == "form12":
+            form_12()
+        elif user_selected_page == "form13":
+            form_13()
 
 if __name__ == "__main__":
     main_form()

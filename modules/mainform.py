e36578335f3dccaa8e4c7e04ba8cc9d0c50d4e9d
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-19 19:13:37 -0400

    Update mainform.py

--- modules/mainform.py
+++ modules/mainform.py
@@ -16,6 +16,24 @@ from .form11 import form_11
 from .form12 import form_12
 from .form13 import form_13
 
+# Create a dictionary to track form submission status
+form_submission_status = {
+    "form0": False,
+    "form1": False,
+    "form2": False,
+    "form3": False,
+    "form4": False,
+    "form5": False,
+    "form6": False,
+    "form7": False,
+    "form8": False,
+    "form9": False,
+    "form10": False,
+    "form11": False,
+    "form12": False,
+    "form13": False,
+}
+
 def main_form():
     form_functions = {
         "form0": form_0,
@@ -35,8 +53,20 @@ def main_form():
     }
     
     user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", list(form_functions.keys()))
-    
+
+    # Check if the form has been submitted successfully
+    if user_selected_page in form_submission_status and form_submission_status[user_selected_page]:
+        st.sidebar.markdown(f"**{user_selected_page} (Completed)**")
+    else:
+        st.sidebar.radio(user_selected_page, list(form_functions.keys()))
+
+    # Call the selected form function
     form_functions.get(user_selected_page, lambda: st.write("Select a form from the sidebar."))()
+
+    # Check if the form was submitted and update the status
+    if st.form_submit_button("Submit Form"):
+        if user_selected_page in form_submission_status:
+            form_submission_status[user_selected_page] = True
 
 if __name__ == "__main__":
     main_form()

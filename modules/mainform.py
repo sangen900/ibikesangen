dd223ff9e5da5912357dc0cfa75266031d83c3c6
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-10-19 18:49:29 -0400

    Update mainform.py

--- modules/mainform.py
+++ modules/mainform.py
@@ -1,4 +1,6 @@
 import streamlit as st
+
+# Import your form functions here with the dot prefix.
 from .form0 import form_0
 from .form1 import form_1
 from .form2 import form_2
@@ -15,49 +17,46 @@ from .form12 import form_12
 from .form13 import form_13
 
 def main_form():
-    page_names1001 = ["form0", "form1", "form2", "form3", "form4", "form5", "form6", "form7", "form8", "form9", "form10", "form11", "form12", "form13"]
-    form_status = {page: False for page in page_names1001}
-    user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", page_names1001)
-
-    form_completed = False
-
-    if user_selected_page == "form0" or user_selected_page == "form3" or user_selected_page == "form5" or user_selected_page == "form8":
-        form_completed = st.checkbox("Form Completed", key=user_selected_page)
-
-    if user_selected_page == "form0":
-        form_0()
-    elif user_selected_page == "form1":
-        form_1()
-    elif user_selected_page == "form2":
-        form_2()
-    elif user_selected_page == "form3":
-        form_3()
-    elif user_selected_page == "form4":
-        form_4()
-    elif user_selected_page == "form5":
-        form_5()
-    elif user_selected_page == "form6":
-        form_6()
-    elif user_selected_page == "form7":
-        form_7()
-    elif user_selected_page == "form8":
-        form_8()
-    elif user_selected_page == "form9":
-        form_9()
-    elif user_selected_page == "form10":
-        form_10()
-    elif user_selected_page == "form11":
-        form_11()
-    elif user_selected_page == "form12":
-        form_12()
-    elif user_selected_page == "form13":
-        form_13()
-
-    if form_completed:
+    page_names = ["form0", "form1", "form2", "form3", "form4", "form5", "form6", "form7", "form8", "form9", "form10", "form11", "form12", "form13"]
+    form_status = {page: False for page in page_names}
+    
+    user_selected_page = st.sidebar.radio("Please read the form carefully and fill the below form.", page_names)
+    
+    # Check if the selected form is one of the auto-completed forms (form0, form3, form5, form8, form13)
+    auto_completed_forms = ["form0", "form3", "form5", "form8", "form13"]
+    
+    # Check if the selected page is an auto-completed form and automatically check the checkbox
+    if user_selected_page in auto_completed_forms:
         form_status[user_selected_page] = True
+    
+    # Display the form corresponding to the selected page
+    form_functions = {
+        "form0": form_0,
+        "form1": form_1,
+        "form2": form_2,
+        "form3": form_3,
+        "form4": form_4,
+        "form5": form_5,
+        "form6": form_6,
+        "form7": form_7,
+        "form8": form_8,
+        "form9": form_9,
+        "form10": form_10,
+        "form11": form_11,
+        "form12": form_12,
+        "form13": form_13
+    }
 
-    if form_status[user_selected_page]:
-        st.success(f"{user_selected_page} marked as completed!")
+    if user_selected_page in form_functions:
+        form_functions[user_selected_page]()
+
+    # Check if the user has submitted the form successfully and update the checkbox
+    if st.button("Submit Form"):
+        if user_selected_page not in auto_completed_forms:
+            form_status[user_selected_page] = True
+
+    # Show the checkbox to manually indicate form completion
+    form_status[user_selected_page] = st.checkbox(f"{user_selected_page} Completed", form_status[user_selected_page])
 
 if __name__ == "__main__":
     main_form()

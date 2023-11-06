95ba7dcf4f928163411b95420ad7566957d5768d
sangen900 <144646886+sangen900@users.noreply.github.com>
2023-11-06 09:04:32 -0500

    normal button

--- modules/form4.py
+++ modules/form4.py
@@ -25,81 +25,81 @@ def form_4():
     });
     </script>
     """
-    with st.form("form5"):
-        st.markdown(
-            """
-            <p style='font-size: 20px; margin-bottom: 0;'>In your own words, define manufacturing.</p>
-            """,
-            unsafe_allow_html=True
-        )
-        user_input1 = st.text_area("", key="text_area_1")
-        st.markdown("---")
-        st.markdown(
-            """
-            <p style='font-size: 20px; margin-bottom: 0;'>In your own words, define manufacturing pillars.</p>
-            """,
-            unsafe_allow_html=True
-        )
-        user_input2 = st.text_area("", key="text_area_2")
-        st.markdown("---")
-        st.markdown(
-            """
-            <p style='font-size: 20px; margin-bottom: 0;'>List and briefly describe the six pillars.</p>
-            """,
-            unsafe_allow_html=True
-        )
-        user_input3 = st.text_area("", key="text_area_3")
-        st.markdown("---")
-        st.markdown(
-            """
-            <p style='font-size: 20px; margin-bottom: 0;'>In your own words, define manufacturing systems.</p>
-            """,
-            unsafe_allow_html=True
-        )
-        user_input4 = st.text_area("", key="text_area_4")
-        st.markdown("---")
-        st.markdown(
-            """
-            <p style='font-size: 20px; margin-bottom: 0;'>In your own words, define manufacturing technologies?</p>
-            """,
-            unsafe_allow_html=True
-        )
-        user_input5 = st.text_area("", key="text_area_5")
-        st.markdown("---")
-        st.markdown(
-            """
-            <p style='font-size: 20px; margin-bottom: 0;'>In your own words, define product development lifecycle?</p>
-            """,
-            unsafe_allow_html=True
-        )
-        user_input6 = st.text_area("", key="text_area_6")
-        #submit button
-        submit_button2 = st.form_submit_button("Submit Form")
-        if submit_button2:
-           if not user_input1 or not user_input2 or not user_input3 or not user_input4 or not user_input5 or not user_input6:
-             warnings.append("Please fill all the required forms:")
-           if warnings:
-                st.warning("\n".join(warnings))
-           else:
-                scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-                credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_path, scope)
-                gc = gspread.authorize(credentials)
-                #gsspreadsheet where data will be stored
-                spreadsheet_id = "1UiBuyoFudQnvgzgIxlh__6ktEBKK7zJvqoWYwz_2WuE"  # Replace with your Spreadsheet ID
-                sheet = gc.open_by_key(spreadsheet_id)
-                worksheet_title = "Sheet3"
-                worksheet = sheet.worksheet(worksheet_title)
-                form_data2 = {
-                            'In your own words, define manufacturing.': user_input1,
-                            'In your own words, define manufacturing pillars.': user_input2,
-                            'List and briefly describe the six pillars.': user_input3,
-                            'In your own words, define manufacturing systems.': user_input4,
-                            'In your own words, define manufacturing technologies?': user_input5,
-                            'In your own words, define product development lifecycle?':user_input6
-                        }
-                num_rows = len(worksheet.get_all_values())
-                worksheet.insert_rows([list(form_data2.values())], num_rows+1)
-                st.success(f"Form 5 has been successfully submitted:")
-                return True
 
-        
+    st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>In your own words, define manufacturing.</p>
+        """,
+        unsafe_allow_html=True
+    )
+    user_input1 = st.text_area("", key="text_area_1")
+    st.markdown("---")
+    st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>In your own words, define manufacturing pillars.</p>
+        """,
+        unsafe_allow_html=True
+    )
+    user_input2 = st.text_area("", key="text_area_2")
+    st.markdown("---")
+    st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>List and briefly describe the six pillars.</p>
+        """,
+        unsafe_allow_html=True
+    )
+    user_input3 = st.text_area("", key="text_area_3")
+    st.markdown("---")
+    st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>In your own words, define manufacturing systems.</p>
+        """,
+        unsafe_allow_html=True
+    )
+    user_input4 = st.text_area("", key="text_area_4")
+    st.markdown("---")
+    st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>In your own words, define manufacturing technologies?</p>
+        """,
+        unsafe_allow_html=True
+    )
+    user_input5 = st.text_area("", key="text_area_5")
+    st.markdown("---")
+    st.markdown(
+        """
+        <p style='font-size: 20px; margin-bottom: 0;'>In your own words, define product development lifecycle?</p>
+        """,
+        unsafe_allow_html=True
+    )
+    user_input6 = st.text_area("", key="text_area_6")
+    #submit button
+    submit_button2 = st.form_submit_button("Submit Form")
+    if submit_button2:
+       if not user_input1 or not user_input2 or not user_input3 or not user_input4 or not user_input5 or not user_input6:
+         warnings.append("Please fill all the required forms:")
+       if warnings:
+            st.warning("\n".join(warnings))
+       else:
+            scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+            credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_path, scope)
+            gc = gspread.authorize(credentials)
+            #gsspreadsheet where data will be stored
+            spreadsheet_id = "1UiBuyoFudQnvgzgIxlh__6ktEBKK7zJvqoWYwz_2WuE"  # Replace with your Spreadsheet ID
+            sheet = gc.open_by_key(spreadsheet_id)
+            worksheet_title = "Sheet3"
+            worksheet = sheet.worksheet(worksheet_title)
+            form_data2 = {
+                        'In your own words, define manufacturing.': user_input1,
+                        'In your own words, define manufacturing pillars.': user_input2,
+                        'List and briefly describe the six pillars.': user_input3,
+                        'In your own words, define manufacturing systems.': user_input4,
+                        'In your own words, define manufacturing technologies?': user_input5,
+                        'In your own words, define product development lifecycle?':user_input6
+                    }
+            num_rows = len(worksheet.get_all_values())
+            worksheet.insert_rows([list(form_data2.values())], num_rows+1)
+            st.success(f"Form 5 has been successfully submitted:")
+            return True
+
+    
